7207f27ae1cb8e10d37cc0e53f3804423f0cb5d2
Deividas Gineitis <deividasgineitis123@gmail.com>
2024-09-23 19:43:56 +0300

    Fix

--- lab1/README.md
+++ lab1/README.md
@@ -8,35 +8,3 @@ To achieve this goal this task entails creating a real world computation simulat
 Imagine you have a system where multiple threads are logging messages to a file.
 Without proper synchronization, the log messages could get mixed up and result in a corrupted log file.
 
-### Correct program output
-
-Thread 1 logging message 0
-Thread 2 logging message 0
-Thread 1 logging message 1
-Thread 1 logging message 2
-Thread 1 logging message 3
-Thread 1 logging message 4
-Thread 2 logging message 1
-Thread 2 logging message 2
-Thread 2 logging message 3
-Thread 2 logging message 4
-
-### Incorrect program output
-
-Thread 1 logging message 0
-Thread 1 logging message 1
-Thread 1 logging message 2
-Thread 1 logging message 3
-Thread Thread 1 logging message 2 logging message 4
-0
-Thread 2 logging message 1
-Thread 2 logging message 2
-Thread 2 logging message 3
-Thread 2 logging message 4
-
-### Notes on the solution
-
-The critical section without mutual exclusion causes data race, so we need to add a synchronization mechanism to it.
-In C++ we have access to the mutex library that easily locks the critical section. I also added a custom mutex implementation.
-In it, I use an atomic flag provided from atomic library, as boolean is not an atomic operation, meaning that it can be changed by other threads,
-thus having the same data race problem.
--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <fstream>
 #include <thread>
 #include <atomic>
-// #include <mutex>
+#include <string>
 
-class CustomMutualExlusion {
+class CustomMutualExclusion {
 private:
     std::atomic_flag flag = ATOMIC_FLAG_INIT;
 
@@ -20,29 +19,57 @@ public:
     }
 };
 
-std::ofstream logfile("log.txt");
-// std::mutex mtx;
-CustomMutualExlusion mtx;
+long long sharedTotal = 1;
+int logIndex = 0;
+std::string sharedLog[100];
+CustomMutualExclusion mtx;
 
-void logMessage(const int threadID) {
-    for (int i = 0; i < 5; ++i) {
-        // std::lock_guard<std::mutex> lock(mtx);
-        mtx.lock();
-        //-------- Critical section ---------------------------------------------
-        logfile << "Thread " << threadID << " logging message " << i << std::endl;
-        //-----------------------------------------------------------------------
-        mtx.unlock();
+void computeSum(const int threadID, const int* data, const int start, const int end, int& logIndex) {
+    long long partial = 0;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+
+    // mtx.lock();
+    //-------- Critical section ---------------------------------------------
+    for (int i = start; i < end; ++i) {
+        partial += data[i];
     }
+
+    sharedLog[logIndex++] = "Thread " + std::to_string(threadID) + " current shared value " + std::to_string(partial);
+    sharedTotal += partial;
+    //-----------------------------------------------------------------------
+    // mtx.unlock();
 }
 
+// incorrect computation - one thread log is not correct
+// Final shared total: 500000500001
+// Log of computations:
+// Thread 2 current shared value 937512500
+// Thread 3 current shared value 1562512500
+// Thread 4 current shared value 497187962500
+
 int main() {
-    std::thread t1(logMessage, 1);
-    std::thread t2(logMessage, 2);
+    int data[1000000];
+    for (int i = 0; i < 1000000; ++i) {
+        data[i] = i + 1;
+    }
+
+    std::thread t1(computeSum, 1, data, 0, 25000, std::ref(logIndex));
+    std::thread t2(computeSum, 2, data, 25000, 50000, std::ref(logIndex));
+    std::thread t3(computeSum, 3, data, 50000, 75000, std::ref(logIndex));
+    std::thread t4(computeSum, 4, data, 75000, 1000000, std::ref(logIndex));
 
     t1.join();
     t2.join();
+    t3.join();
+    t4.join();
 
-    logfile.close();
+    std::cout << "Final shared total: " << sharedTotal << std::endl;
+    std::cout << "Log of computations:" << std::endl;
+
+    for(const auto & i : sharedLog) {
+        std::cout << i << std::endl;
+    }
 
     return 0;
 }
