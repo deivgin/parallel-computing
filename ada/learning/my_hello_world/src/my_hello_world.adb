cd548f46b2acba924b25ad9fd1b0a66d71392300
Deividas Gineitis <deividasgineitis123@gmail.com>
2024-10-06 19:43:47 +0300

    Loop and proper formatting

--- ada/learning/my_hello_world/src/my_hello_world.adb
+++ ada/learning/my_hello_world/src/my_hello_world.adb
@@ -1,6 +1,10 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 procedure My_Hello_World is
+   N : Integer := 0;
 begin
-   Put_Line ("Hello, World!!");
+   while N < 10 loop
+      Put_Line ("Hello, World!!" & Integer'Image (N));
+      N := N + 1;
+   end loop;
 end My_Hello_World;
