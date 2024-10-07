3f8f3639ff8529d65851f35cfb1c788c557926d5
Deividas Gineitis <deividasgineitis123@gmail.com>
2024-10-07 18:56:47 +0300

    Remove comments, update readme

--- lab2/README.md
+++ lab2/README.md
@@ -5,3 +5,8 @@ for when all threads have finished their work.
 
 The test is done by simulating image processing - each thread works on an image chunk.
 We then wait when all chunks are done to continue on other processes.
+
+Min parts of the code:
+1. Barrier - class that makes threads wait and not continue work until all threads have finished.
+2. processImageChunk - function that simulates image chunk processing.
+3. threadImageProcessing - function that simulates a certain image process action, like filter.
--- lab2/main.cpp
+++ lab2/main.cpp
@@ -14,12 +14,10 @@ public:
         ++counter;
 
         if (counter == threadCount) {
-            // Reset on last thread
             counter = 0;
             ++counter;
             cv.notify_all();
         } else {
-            // Wait until all threads reach the barrier
             cv.wait(lock, [this, currentGeneration] {
                 return counter != currentGeneration;
             });
@@ -34,7 +32,6 @@ private:
     int counter;
 };
 
-// Simulating image processing: each thread works on a chunk of the image
 void processImageChunk(const int threadID, const int chunkID) {
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -46,15 +43,14 @@ void processImageChunk(const int threadID, const int chunkID) {
     std::cout << "Thread " << threadID << " processed chunk " << chunkID << " in " << workTime << " ms.\n";
 }
 
-// Each thread simulates processing of an image part in several steps
 void threadImageProcessing(Barrier& barrier, const int threadID, const int chunks, const int steps) {
     for (int step = 1; step <= steps; ++step) {
         for (int chunkID = 1; chunkID <= chunks; ++chunkID) {
-            processImageChunk(threadID, chunkID);  // Process a chunk of the image
+            processImageChunk(threadID, chunkID);
         }
 
         std::cout << "Thread " << threadID << " finished step " << step << ". Waiting for others...\n";
-        barrier.waitBarrier();  // Synchronize threads at the end of the step
+        barrier.waitBarrier();
         std::cout << "Thread " << threadID << " continues to next step after step " << step << ".\n";
     }
 }
@@ -66,7 +62,6 @@ int main() {
 
     std::vector<std::thread> threads;
 
-    // Each thread simulates processing parts of the image
     for (int i = 0; i < numThreads; ++i) {
         constexpr int steps = 3;
         constexpr int chunks = 2;
@@ -74,7 +69,6 @@ int main() {
         threads.emplace_back(threadImageProcessing, std::ref(barrier), i, chunks, steps);
     }
 
-    // Join all threads after they finish
     for (auto& th : threads) {
         th.join();
     }
